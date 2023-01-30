cd17f1c004f8f8be80f00452b1836c0fcd11ac61
Count391 <59774798+Count391@users.noreply.github.com>
2023-01-30 10:20:05 -0800

    Update Gamebar.ino
    Logic Debug

--- Gamebar.ino
+++ Gamebar.ino
@@ -28,18 +28,19 @@ void loop() {
     digitalWrite((currentLED + 26), LOW);
     currentLED = nextLED;
     digitalWrite((currentLED + 26), HIGH);
+    digitalWrite
   }
-  if(!digitalRead(22)||currentLED == 1){        //Check input against target
-    nextLED = random(1, 7);                     //Change pattern if input matches target
-  }else if(!digitalRead(23)||currentLED == 2){
-    nextLED = random(1, 7);
-  }else if(!digitalRead(24)||currentLED == 3){
-    nextLED = random(1, 7);
-  }else if(!digitalRead(25)||currentLED == 4){
-    nextLED = random(1, 7);
-  }else if(!digitalRead(26)||currentLED == 5){
-    nextLED = random(1, 7);
-  }else if(!digitalRead(27)||currentLED == 6){
-    nextLED = random(1, 7);
+  if(!digitalRead(22)&&currentLED == 1){        //Check input against target
+    nextLED = random(1, 6);                     //Change pattern if input matches target
+  }else if(!digitalRead(23)&&currentLED == 2){
+    nextLED = random(1, 6);
+  }else if(!digitalRead(24)&&currentLED == 3){
+    nextLED = random(1, 6);
+  }else if(!digitalRead(25)&&currentLED == 4){
+    nextLED = random(1, 6);
+  }else if(!digitalRead(26)&&currentLED == 5){
+    nextLED = random(1, 6);
+  }else if(!digitalRead(27)&&currentLED == 6){
+    nextLED = random(1, 6);
   }
 }
