1132365faec176fbba3f8f80f18ea6107420a97e
Count391 <59774798+Count391@users.noreply.github.com>
2023-03-19 17:04:12 -0700

    Add files via upload

--- CombinedController.ino
+++ CombinedController.ino
@@ -15,9 +15,9 @@ ModbusTCPClient modbusTCPClient(ethClient);
 
 //Buttons setup
 int enablePin = 7;    //Power button
-int startPin = 8;     //play/pause button
+int playpause = 2;     //play/pause button
 int timeUpPin = 9;    //increase time
-int timeDownPin = 10; //decrease time
+//int timeDownPin = 10; //decrease time
 int angle1Pin = 11;   //angle dial
 int angle2Pin = 12;
 int angle3Pin = 13;
@@ -30,6 +30,10 @@ int autoPin = A3;     //auto mode
 int outputAngleValue = 1;
 int outputSpeedValue = 1;
 int time = 0;
+byte lastButtonState = LOW;
+unsigned long debounceDuration = 50; // millis
+unsigned long lastTimeButtonStateChanged = 0;
+int count = 1;
 
 enum dir {
   pos,
@@ -39,9 +43,9 @@ enum dir {
 void setup() {
   //Buttons setup
   pinMode(enablePin, INPUT_PULLUP);
-  pinMode(startPin, INPUT_PULLUP);
+  pinMode(playpause, INPUT_PULLUP);
   pinMode(timeUpPin, INPUT_PULLUP);
-  pinMode(timeDownPin, INPUT_PULLUP);
+  //pinMode(timeDownPin, INPUT_PULLUP);
   pinMode(angle1Pin, INPUT_PULLUP);
   pinMode(angle2Pin, INPUT_PULLUP);
   pinMode(angle3Pin, INPUT_PULLUP);
@@ -67,29 +71,68 @@ void setup() {
 
 void loop() {
   //Modbus Loop
-  if(!modbusTCPClient.connected()){
+  if (!modbusTCPClient.connected()) {
     Serial.println("Attempting to connect to Modbus TCP server");
 
     if (!modbusTCPClient.begin(server, 502)) {
       Serial.println("Modbus TCP Client failed to connect!");
     } else {
       Serial.println("Modbus TCP Client connected");
+      enableAxes();
+      setVelLvl(2);
+      setAngle(2);
     }
   } else {
-    
+
   }
+  if (millis() - lastTimeButtonStateChanged > debounceDuration) {
+      byte buttonState = digitalRead(playpause);
+      if (buttonState != lastButtonState) {
+        lastTimeButtonStateChanged = millis();
+        lastButtonState = buttonState;
+        if (buttonState == LOW) {          //button has been pressed
+          if(count == 1){
+            toggleMotion();
+            count = 2;
+          }
+          else{
+            disableAxes();
+            count = 1;
+          }
+          Serial.println("Button pressed");
+        }
+      }
+  }
+//  jog(1, neg);
+//  jog(2, neg);
+//  Serial.println("Velocity Level 1, Angle Level 1");
+//  delay(15000);
+//  disableAxes();
+//  setVelLvl(2);
+//  setAngle(2);
+//  enableAxes();
+//  Serial.println("Velocity Level 2, Angle Level 2");
+//  delay(15000);
+//  disableAxes();
+//  setVelLvl(3);
+//  setAngle(3);
+//  enableAxes();
+//  Serial.println("Velocity Level 3, Angle Level 3");
+//  delay(15000);
+//  disableAxes();
+//  Serial.println("Axes Disabled");
 
   //Button Loop
   //while power button is ON
-    //define angle, speed, time, axes, bounce
-    //enable motors
-    // home
-    //how do we do these two only once? 
-    //while play/pause is ON
-      //start timer
-      //if timer is less or equal to 0
-      //ending sequence
-    //remember the time we were at when paused
+  //define angle, speed, time, axes, bounce
+  //enable motors
+  // home
+  //how do we do these two only once?
+  //while play/pause is ON
+  //start timer
+  //if timer is less or equal to 0
+  //ending sequence
+  //remember the time we were at when paused
 
 }
 
@@ -98,33 +141,52 @@ void setAngle(int lvl) {
   // if level 1 use setLimits for predefined value
   // if level 2 ...
   // if level 3 ...
+  if (lvl == 1) {
+    setLimits(1500, 2000);
+  }
+  else if (lvl == 2) {
+    setLimits(750, 2000);
+  }
+  else if (lvl == 3) {
+    setLimits(100, 2000);
+  }
 }
 
 //set limits
-void setLimits(int lp, int ln) {
+void setLimits(int ln, int lp) {
   //AXIS 1 Limits
   //108-109 is upper limit
   int address = 108;
-  writeTwoRegisters(address,lp);
+  writeTwoRegisters(address, lp);
   //110-111 is lower limit
   address = 110;
-  writeTwoRegisters(address,ln);
+  writeTwoRegisters(address, ln);
   //AXIS 2 Limits
   //124-125 is upper limit
   address = 124;
-  writeTwoRegisters(address,lp);
+  writeTwoRegisters(address, lp);
   //126-127 is lower limit
   address = 126;
-  writeTwoRegisters(address,ln);
+  writeTwoRegisters(address, ln);
+  enableActions(); // ACTIONS disable whenever a parameter is changed
 }
 
-void setVelLvl(int lvl){
+void setVelLvl(int lvl) {
   // if level 1 use setVelocity for predefined value
   // if level 2 ...
   // if level 3 ...
+  if (lvl == 1) {
+    setVelocity(300);
+  }
+  else if (lvl == 2) {
+    setVelocity(800);
+  }
+  else if (lvl == 3) {
+    setVelocity(1200);
+  }
 }
 
-void setVelocity(int vel){
+void setVelocity(int vel) {
   //set velocity in ACTIONS(task parameters)
   //action 13 = neg, 14 = pos for AXIS 1 (address 112-113, 114-115)
   //action 15 = neg, 16 = pos for AXIS 2 (address 128-129, 130-131)
@@ -132,58 +194,59 @@ void setVelocity(int vel){
   int axis1pos = 114;
   int axis2neg = 128;
   int axis2pos = 130;
-  writeTwoRegisters(axis1neg,-vel);
-  writeTwoRegisters(axis1pos,vel);
-  writeTwoRegisters(axis2neg,-vel);
-  writeTwoRegisters(axis2pos,vel);
+  writeTwoRegisters(axis1neg, -vel);
+  writeTwoRegisters(axis1pos, vel);
+  writeTwoRegisters(axis2neg, -vel);
+  writeTwoRegisters(axis2pos, vel);
   //set general JOG velocities
   //AXIS1.JOG.V is at 118 and 119
   //AXIS1.JOG.V is at 120 and 121
   int axis1 = 118;
   int axis2 = 120;
-  writeTwoRegisters(axis1,vel);
-  writeTwoRegisters(axis2,vel);
+  writeTwoRegisters(axis1, vel);
+  writeTwoRegisters(axis2, vel);
+  enableActions();  // ACTIONS disable whenever a parameter is changed
 }
 
-void writeTwoRegisters(int address, int value){
-  value = value*1000;
-  int value1 = value/(16^4);
-  int value2 = value - value1*(16^4);  
-  modbusTCPClient.holdingRegisterWrite(address-1,value1);
-  modbusTCPClient.holdingRegisterWrite(address,value2);
+void writeTwoRegisters(int address, int value) {
+  long v = value * 1000.00;
+  long value1 = v >> 16;
+  long value2 = v - (value1 << 16);
+  modbusTCPClient.holdingRegisterWrite(address - 1, int(value1));
+  modbusTCPClient.holdingRegisterWrite(address, int(value2));
 }
 
 void enableAxes() {
   int axis1 = 96;
   int axis2 = 98;
-  modbusTCPClient.holdingRegisterWrite(axis1-1,1); //set ENABLE to high, then IMMEDIATELY set it back to low
-  modbusTCPClient.holdingRegisterWrite(axis1-1,0);
-  modbusTCPClient.holdingRegisterWrite(axis2-1,1);
-  modbusTCPClient.holdingRegisterWrite(axis2-1,0);
+  modbusTCPClient.holdingRegisterWrite(axis1 - 1, 1); //set ENABLE to high, then IMMEDIATELY set it back to low
+  modbusTCPClient.holdingRegisterWrite(axis1 - 1, 0);
+  modbusTCPClient.holdingRegisterWrite(axis2 - 1, 1);
+  modbusTCPClient.holdingRegisterWrite(axis2 - 1, 0);
 }
 
 void disableAxes() {
   int axis1 = 97;
   int axis2 = 99;
-  modbusTCPClient.holdingRegisterWrite(axis1-1,1); //set ENABLE to high, then IMMEDIATELY set it back to low
-  modbusTCPClient.holdingRegisterWrite(axis1-1,0);
-  modbusTCPClient.holdingRegisterWrite(axis2-1,1);
-  modbusTCPClient.holdingRegisterWrite(axis2-1,0);
+  modbusTCPClient.holdingRegisterWrite(axis1 - 1, 1); //set ENABLE to high, then IMMEDIATELY set it back to low
+  modbusTCPClient.holdingRegisterWrite(axis1 - 1, 0);
+  modbusTCPClient.holdingRegisterWrite(axis2 - 1, 1);
+  modbusTCPClient.holdingRegisterWrite(axis2 - 1, 0);
 }
 
 void jog(int axis, dir pn) { // will need to specify which axis and pos/neg
   int address = 100;
-  if(axis == 2){
-    int address = 101;
+  if (axis == 2) {
+    address = 102;
   }
-  if(pn == neg){
+  if (pn == neg) {
     address = address + 1;
   }
-  modbusTCPClient.holdingRegisterWrite(address-1,1); //set ENABLE to high, then IMMEDIATELY set it back to low
-  modbusTCPClient.holdingRegisterWrite(address-1,0);
+  modbusTCPClient.holdingRegisterWrite(address - 1, 1); //set ENABLE to high, then IMMEDIATELY set it back to low
+  modbusTCPClient.holdingRegisterWrite(address - 1, 0);
 }
 
-int actionRunning(){
+int actionRunning() {
   int address1 = 104;
   int address2 = 105;
   int data1 = modbusTCPClient.holdingRegisterRead(address1);
@@ -192,17 +255,26 @@ int actionRunning(){
   return result;
 }
 
-void enableCmdSource(){
+void enableCmdSource() {
   int address = 116;
-  modbusTCPClient.holdingRegisterWrite(address-1,1);
-  modbusTCPClient.holdingRegisterWrite(address,1);
+  modbusTCPClient.holdingRegisterWrite(address - 1, 1);
+  modbusTCPClient.holdingRegisterWrite(address, 1);
 }
 
-void enableActions(){ //should only need to do this once at start
+
+//MUST ENABLE ACTIONS AFTER CHANGING PARAMETER OR CONDITION VALUE
+void enableActions() { //should only need to do this once at start
   int address = 106;  // for ACTION 13 & 14
-  modbusTCPClient.holdingRegisterWrite(address-1,1);
-  modbusTCPClient.holdingRegisterWrite(address,1);
+  modbusTCPClient.holdingRegisterWrite(address - 1, 1);
+  modbusTCPClient.holdingRegisterWrite(address, 1);
   address = 122;      // for ACTION 15 & 16
-  modbusTCPClient.holdingRegisterWrite(address-1,1);
-  modbusTCPClient.holdingRegisterWrite(address,1);
+  modbusTCPClient.holdingRegisterWrite(address - 1, 1);
+  modbusTCPClient.holdingRegisterWrite(address, 1);
+}
+
+void toggleMotion(){
+  enableAxes();
+  delay(50);
+  jog(1,pos);
+  jog(2,pos);
 }
