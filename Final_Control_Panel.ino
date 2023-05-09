8d975485cc837d384dec0dc07abe51a2020e275a
Count391 <59774798+Count391@users.noreply.github.com>
2023-05-08 17:16:19 -0700

    Update Final_Control_Panel.ino

--- Final_Control_Panel.ino
+++ Final_Control_Panel.ino
@@ -1,16 +1,18 @@
 // Import necessary libraries for 7-segment display
 #include <Wire.h> 
 #include <Adafruit_GFX.h>
+#include <ezButton.h>
 #include "Adafruit_LEDBackpack.h"
 
 Adafruit_7segment matrix = Adafruit_7segment();
 
 //Assign pins
-#define powerPin 7
-#define pausePin 10
-#define bouncePin 5
-#define motionPin 9
-#define axisPin 11
+ezButton power(7);  // Power
+ezButton play(10);  // Play/Pause
+ezButton bounce(5); // Bounce Toggle
+ezButton motion(9);   // Baby/Auto Mode
+ezButton axis(11);  // Toggle 3 axis choices
+ezButton timer(12); // Timer press in
 #define angle_DT A5
 #define angle_CLK A4
 #define speed_DT A3
@@ -22,40 +24,33 @@ Adafruit_7segment matrix = Adafruit_7segment();
 #define register_SER 17 
 #define ledPin 13
 #define com 1
-#define latchPin 19
-#define clockPin 18
-#define dataPin 17
+#define latchPin 4
+#define clockPin 6
+#define dataPin 8
 
 // Initializing variables
-int outputAngleValue = 0;
-int outputSpeedValue = 0;
+int timer_state = 0;  //Crono = 1, stopwatch = 0
+int outputAngleValue = 1;
+int outputSpeedValue = 1;
 int state = 0;
 long message = 0;
 /*
  * message bit meaning from left to right:
- * 1. On/Off
- * 2.Speed
- * 3.Angle
- * 4. axis
- * 5. Motion type
+ * 1. Motion type
+ * 2. Axis
+ * 3. Angle
+ * 4. Speed
+ * 5. Bounce
  */
-boolean power = 0;
-boolean start = 0;
-int axis = 0; //0 for pitch, 1 for roll, 2 for combine
-boolean motion = 0; //0 for auto mode, 1 for baby mode
+boolean powerState = 0;
+boolean startState = 0;
+int axisState = 0; //0 for pitch, 1 for roll, 2 for combine
+boolean motionState = 0; //0 for auto mode, 1 for baby mode
 long duration = 845000;
 boolean speedRotaryState = 0;
 boolean angleRotaryState = 0;
 boolean timeRotaryState = 0;
-
-boolean button(int pinNumber){
-  byte mask = 0;
-  for (int i = 1; i <= 4; i++){
-    mask = mask << 1;
-    mask = mask + digitalRead(pinNumber);
-  }
-  return (mask == 15);
-}
+int bounceState = 0; //0 for off, 1 for on
 
 int rotary(int CLK, int DT, boolean lastStateCLK){
   boolean currentStateCLK = digitalRead(CLK);
@@ -72,27 +67,29 @@ int rotary(int CLK, int DT, boolean lastStateCLK){
     }
 }
 
-byte smallLed(int speedValue, int angleValue, int motion){
+byte smallLed(int speedValue, int angleValue, int axis){
   int ledRegister = 0;
   if (speedValue > 1){
+    bitSet(ledRegister, 1);
+  }
+  if (speedValue == 3){
     bitSet(ledRegister, 2);
-  }else if (speedValue == 3){
-    bitSet(ledRegister, 3);
   }
   if (angleValue > 1){
-    bitSet(ledRegister, 4);
-  }else if (speedValue == 3){
-    bitSet(ledRegister, 5);
+    bitSet(ledRegister, 3);
   }
-  switch (motion){
+  if (angleValue == 3){
+    bitSet(ledRegister, 4);
+  }
+  switch (axis){
     case 1:
-    bitSet(ledRegister, 6);
+    bitSet(ledRegister, 5);
     break;
     case 2:
-    bitSet(ledRegister, 7);
+    bitSet(ledRegister, 6);
     break;
     default:
-    bitSet(ledRegister, 8);
+    bitSet(ledRegister, 7);
     break;
   }
   return ledRegister;
@@ -101,11 +98,12 @@ byte smallLed(int speedValue, int angleValue, int motion){
 // Setup function
 void setup() {
   Serial.begin(9600);
-  pinMode(powerPin, INPUT_PULLUP);
-  pinMode(pausePin, INPUT_PULLUP);
-  pinMode(bouncePin, INPUT_PULLUP);
-  pinMode(motionPin, INPUT_PULLUP);
-  pinMode(axisPin, INPUT_PULLUP);
+  power.setDebounceTime(50); // set debounce time to 50 milliseconds
+  play.setDebounceTime(50);
+  bounce.setDebounceTime(50);
+  motion.setDebounceTime(50);
+  axis.setDebounceTime(50);
+  timer.setDebounceTime(50);
   pinMode(angle_DT, INPUT);
   pinMode(angle_CLK, INPUT);
   pinMode(speed_DT, INPUT);
@@ -127,78 +125,80 @@ boolean lastStateTimeCLK = digitalRead(time_CLK);
 
 // Loop function
 void loop() {
+  power.loop(); // MUST call the loop() function first
+  play.loop();
+  bounce.loop();
+  motion.loop();
+  axis.loop();
+  timer.loop();
   byte ledRegister = 0;
   
-  power = digitalRead(powerPin);
+  powerState = !(power.getState());
 
-  if (power){
-    ledRegister = smallLed(outputSpeedValue, outputAngleValue, motion);
-    bitSet(ledRegister, 1);
-    if (button(pausePin)){
-      start = !start;
+  if (powerState){
+    ledRegister = smallLed(outputSpeedValue, outputAngleValue, axisState);
+    bitSet(ledRegister, 0);
+    if (play.isReleased()){
+      startState = !startState;
     }
-    if (start == 0){
-      if (button(motionPin)){
-        motion = !motion;
-      }
-      if (button(axisPin)){
-        if (axis < 3){
-          axis++;
+    if (!startState){
+      motionState = motion.getState();
+      if (axis.isReleased()){
+        if (axisState < 2){
+          axisState++;
         }else{
-          axis = 0;
+          axisState = 0;
         }
       }
+      bounceState = bounce.getState();
     }
   }
   
-  if (power){
+  if (powerState){
     state = 1;
     digitalWrite(latchPin, LOW);
     shiftOut(dataPin, clockPin, LSBFIRST, ledRegister);
     digitalWrite(latchPin, HIGH);
     digitalWrite(ledPin, HIGH);
-    if (start){
+    if (startState){
       state = 2;
-      if (axis == 1){
+      if (axisState == 1){
         state = 3;
-      }else if (axis == 2){
+      }else if (axisState == 2){
         state = 4;
       }
-      if (motion){
+      if (motionState){
         state = 5;
       }
     }else{
-      if (rotary(angle_CLK,angle_DT,lastStateAngleCLK) == 1){
-        if (outputAngleValue < 3){
-          outputAngleValue++;
-          lastStateAngleCLK = !lastStateAngleCLK;
-        }
+      int angle_direction = rotary(angle_CLK, angle_DT, lastStateAngleCLK);
+      int speed_direction = rotary(speed_CLK, speed_DT, lastStateSpeedCLK);
+      lastStateAngleCLK = digitalRead(angle_CLK);
+      lastStateSpeedCLK = digitalRead(speed_CLK);
+      if (angle_direction == 1 && outputAngleValue < 3){
+        outputAngleValue++;
       }
-      if (rotary(angle_CLK,angle_DT,lastStateAngleCLK) == -1){
-        if (outputAngleValue >1){
-          outputAngleValue--;
-          lastStateAngleCLK = !lastStateAngleCLK;
-        }
+      if (angle_direction == -1 && outputAngleValue >1){
+        outputAngleValue--;
       }
-      if (rotary(speed_CLK,speed_DT,lastStateSpeedCLK) == 1){
-        if (outputSpeedValue < 3){
-          outputSpeedValue++;
-          lastStateSpeedCLK = !lastStateSpeedCLK;
-        }
+      if (speed_direction == 1 && outputSpeedValue < 3){
+        outputSpeedValue++;
       }
-      if (rotary(speed_CLK,speed_DT,lastStateSpeedCLK) == -1){
-        if (outputSpeedValue < 3){
-          outputSpeedValue--;
-          lastStateSpeedCLK = !lastStateSpeedCLK;
-        }
+      if (speed_direction == -1 && outputSpeedValue >1){
+        outputSpeedValue--;
       }
     }
   }else{
     outputSpeedValue = 1;
     outputAngleValue = 1;
-    start = 0;
+    startState = 0;
     state = 1;
     duration = 845000;
+    digitalWrite(ledPin, LOW);
+    ledRegister = 0;
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, ledRegister);
+    digitalWrite(latchPin, HIGH);
   }
 
   switch (state){
@@ -206,88 +206,17 @@ void loop() {
     message = 0;
     break;
     case 2:
-    message = 1 + outputSpeedValue * 10 + outputAngleValue * 100;
+    message = bounceState + outputAngleValue * 100 + outputSpeedValue * 10;
     break;
     case 3:
-    message = 1 + outputSpeedValue * 10 + outputAngleValue * 100 + 1000;
+    message = bounceState + outputAngleValue * 100 + outputSpeedValue * 10 + 1000;
     break;
     case 4:
-    message = 1 + outputSpeedValue * 10 + outputAngleValue * 100 + 2000;
+    message = bounceState + outputAngleValue * 100 + outputSpeedValue * 10 + 2000;
     break;
     case 5:
-    message = 1 + 10000;
+    message = 10000;
     break;
   }
   Serial.println(message);
-
-  /*
-  if (button(enablePin)) {
-      matrix.drawColon(true);
-      matrix.writeDisplay();
-      // Check angle pins
-      Serial.print("Angle:");
-      Serial.println(outputAngleValue);
-      Serial.print("Speed:");
-      Serial.println(outputSpeedValue);
-      delay(1000);
-      
-      if (!button(speed1Pin)) {
-        if (button(speed2Pin)){
-          outputSpeedValue = 1;
-        }else{
-          outputSpeedValue  = 2;
-        }
-      } else{
-        outputSpeedValue = 3;
-      }
-      // Check speed pins
-            if (!button(angle1Pin)) {
-        if (button(angle2Pin)){
-          outputAngleValue = 1;
-        }else{
-          outputAngleValue  = 2;
-        }
-      } else{
-        outputAngleValue = 3;
-      }
-      int durationMin = duration / 60000;
-      int ledOutput = durationMin*100 + (duration / 1000) - durationMin * 60;
-      ledDisplay(ledOutput);
-      // Check start pin
-      if (button(startPin)) {
-        delay(1000);
-        // Check if time is larger than 0
-        if (duration > 0) {
-          // Check if auto pin is 1
-            long previousTime = millis();
-            while (button(enablePin) && !button(startPin) && duration > 0){
-              matrix.drawColon(true);
-              matrix.writeDisplay();
-              long currentTime = millis();
-              Serial.println("on");
-              delay(1000);
-              duration -= currentTime - previousTime;
-              previousTime = currentTime;
-              int durationMin = duration / 60000;
-              int ledOutput = durationMin*100 + (duration / 1000) - durationMin * 60;
-              ledDisplay(ledOutput);
-            }
-            
-          } else {
-            int previousTime = 1/1000 * millis();
-            while (button(enablePin) && !button(startPin) && duration > 0){
-              Serial.println("Gamebar");
-              int currentTime = 1/1000 * millis();
-              duration -= currentTime - previousTime;
-              previousTime = currentTime;
-            }
-          }
-          delay(1250);               
-        }
-
-      }else{
-        matrix.print("");
-        matrix.writeDisplay();
-      }
-      */
   }
