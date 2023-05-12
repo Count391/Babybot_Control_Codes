77ac76ad7a5a3a08c1bc2d5d2b8eb61a4e0ccdbc
cqpham101 <62918103+cqpham101@users.noreply.github.com>
2023-05-12 14:35:21 -0700

    Update GamebarFlash.ino

--- GamebarFlash.ino
+++ GamebarFlash.ino
@@ -1,42 +1,42 @@
 #include <ezButton.h>
 
-#define ledPin1 A1
-#define ledPin2 A2
-#define ledPin3 A3
-#define ledPin4 A4
-#define ledPin5 A5
+#define ledPin1 A5 // Green
+#define ledPin2 A4 // Red
+#define ledPin3 A3 // White
+#define ledPin4 A2 // Blue
+#define ledPin5 A1 // Yellow
 
-ezButton button1(7);
-ezButton button2(2);
-ezButton button3(13);
-ezButton button4(12);
-ezButton button5(11);
+ezButton button1(7); // Green
+ezButton button2(2); // Red
+ezButton button3(13); // White
+ezButton button4(12); // Blue
+ezButton button5(11); // Yellow
 
 int currentLED = 1;
 int nextLED;
 
-static const uint8_t pinNumberOutput[] = {A1, A2, A3, A4, A5};
+static const uint8_t pinNumberOutput[] = {A5, A4, A3, A2, A1};
 
 int lastToggle = millis();
 int currentTime;
 int LEDstate = LOW;
 
-void toggleLED(int index){
+void toggleLED(){
   currentTime = millis();
   if(currentTime - lastToggle > 1000){
-    if( LEDstate == LOW)
+    if(LEDstate == LOW)
       LEDstate = HIGH;
     else
       LEDstate = LOW;
     lastToggle = millis();
-    digitalWrite(pinNumberOutput[index],LEDstate);
+    digitalWrite(pinNumberOutput[currentLED-1],LEDstate);
   }
 }
 
 void newLED(){
-    nextLED = random(1,5);
-    if(nextLED == currentLED)
-      newLED();
+    nextLED = random(1,6);
+    while(nextLED == currentLED)
+      nextLED = random(1,6);
     currentLED = nextLED;
 }
 
@@ -52,29 +52,70 @@ void setup() {
   button3.setDebounceTime(50);
   button4.setDebounceTime(50);
   button5.setDebounceTime(50);
+
+  Serial.begin(9600);
+  Serial.print("Current button: ");
+  Serial.println(currentLED);
 }
 
 void loop() {
-//  button1.loop();
-//  button2.loop();
-//  button3.loop();
-//  button4.loop();
-//  button5.loop();
-//
-//  toggleLED(currentLED);
-//
-//
-//  if(button1.isReleased() && currentLED == 1)
-//    newLED();
-//  else if(button2.isReleased() && currentLED == 2)
-//    newLED();
-//  else if(button3.isReleased() && currentLED == 3)
-//    newLED();
-//  else if(button4.isReleased() && currentLED == 4)
-//    newLED();
-//  else if(button5.isReleased() && currentLED == 5)
-//    newLED();
 
-  digitalWrite(ledPin5,HIGH);
+button1.loop();
+button2.loop();
+button3.loop();
+button4.loop();
+button5.loop();
 
+int btn1State = button1.getState();
+int btn2State = button2.getState();
+int btn3State = button3.getState();
+int btn4State = button4.getState();
+int btn5State = button5.getState();
+
+// currently not toggling lights on/off  
+  
+if(button1.isPressed() && currentLED == 1){
+  Serial.println("The button 1 is released");
+  digitalWrite(ledPin1,LOW);
+  newLED(); 
+  digitalWrite(pinNumberOutput[currentLED-1],HIGH); 
+  Serial.print("Current button: ");
+  Serial.println(currentLED);
+}
+
+if(button2.isPressed() && currentLED == 2){
+  Serial.println("The button 2 is released");
+  digitalWrite(ledPin2,LOW);
+  newLED();
+  digitalWrite(pinNumberOutput[currentLED-1],HIGH);
+  Serial.print("Current button: ");
+  Serial.println(currentLED);
+}
+
+if(button3.isPressed() && currentLED == 3){
+  Serial.println("The button 3 is released");
+  digitalWrite(ledPin3,LOW);
+  newLED();
+  digitalWrite(pinNumberOutput[currentLED-1],HIGH);
+  Serial.print("Current button: ");
+  Serial.println(currentLED);
+}
+
+if(button4.isPressed() && currentLED == 4){
+  Serial.println("The button 4 is released");
+  digitalWrite(ledPin4,LOW);
+  newLED();
+  digitalWrite(pinNumberOutput[currentLED-1],HIGH);
+  Serial.print("Current button: ");
+  Serial.println(currentLED);
+}
+
+if(button5.isPressed() && currentLED == 5){
+  Serial.println("The button 5 is released");
+  digitalWrite(ledPin5,LOW);
+  newLED();
+  digitalWrite(pinNumberOutput[currentLED-1],HIGH);
+  Serial.print("Current button: ");
+  Serial.println(currentLED);
+}
 }
